b9eadbe0367621fa2bbdffbfda59df44faf3ca91
Mesbah Mowlavi <m@ionx.ca>
2016-02-17 23:51:50 -0500

    Fixed sys_read errors

--- kern/syscall/read_syscall.c
+++ kern/syscall/read_syscall.c
@@ -9,9 +9,11 @@
 #include <types.h>
 #include <vnode.h>
 #include <vfs.h>
+#include <uio.h>
 #include <copyinout.h>
 #include <limits.h>
 #include <current.h>
+#include <proc.h>
 
 ssize_t sys_read(int fd, void *buf, size_t buflen, int *error){
  	struct file *f;
@@ -20,25 +22,25 @@ ssize_t sys_read(int fd, void *buf, size_t buflen, int *error){
  	*error = 0;
 
  	// get the file struct from the filetable
- 	*error = filetable_get(curproc->p_ft, fd, f);
+ 	*error = filetable_get(curproc->p_ft, fd, &f);
  	if (*error) return -1;
 
  	// set up the uio for reading
- 	i->iov_kbase = buf;
-	i->iov_len = buflen;
-	u->uio_iov = i;
-	u->uio_iovcnt = 1;
-	u->uio_offset = f->f_cursor;
-	u->uio_resid = buflen;
-	u->uio_segflg = UIO_USERSPACE;
-	u->uio_rw = UIO_READ;
-	u->uio_space = NULL;
+ 	i.iov_kbase = buf;
+	i.iov_len = buflen;
+	u.uio_iov = &i;
+	u.uio_iovcnt = 1;
+	u.uio_offset = f->f_cursor;
+	u.uio_resid = buflen;
+	u.uio_segflg = UIO_USERSPACE;
+	u.uio_rw = UIO_READ;
+	u.uio_space = NULL;
 
 	// acquire the lock and do the read
 	lock_acquire(f->f_lock);
 	*error = VOP_READ(f->f_vn, &u);
-	ssize_t bytesRead = u->uio_offset - f->f_cursor;
-	f->f_cursor = u->uio_offset;
+	ssize_t bytesRead = u.uio_offset - f->f_cursor;
+	f->f_cursor = u.uio_offset;
 	lock_release(f->f_lock);
 
 	if (*error) return -1;
