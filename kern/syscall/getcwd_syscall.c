3f0897b4272daa74826ee30afb303612f9beee80
Mesbah Mowlavi <m@ionx.ca>
2016-02-17 23:33:34 -0500

    fixed typos in getcwd

--- kern/syscall/getcwd_syscall.c
+++ kern/syscall/getcwd_syscall.c
@@ -12,26 +12,15 @@ int
 sys___getcwd (char *buf, size_t buflen, int *error)
 {
   	struct iovec iov;
-  	struct uio u;
+  	struct uio uio;
   	int result;
-
-	// Create uio for data transfer
-	iov->iov_kbase = buf;
-	iov->iov_len = buflen;
-	uio->uio_iov = iov;
-	uio->uio_iovcnt = 1;
-	uio->uio_offset = 0;
-	uio->uio_resid = buflen;
-	uio->uio_segflg = UIO_SYSSPACE;
-	uio->uio_rw = UIO_READ;
-	uio->uio_space = NULL;
 	
-	//void uio_kinit(&iov, &uio, buf, buflen, 0, UIO_READ);
+	uio_kinit(&iov, &uio, buf, buflen, 0, UIO_READ);
 
 	// Return result
-	result = vfs_getcwd(uio);
+	result = vfs_getcwd(&uio);
 	if (result){
-		error = result;
+		*error = result;
 		return -1;
 	}
 	return result;
