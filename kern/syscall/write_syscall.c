2967ed31ae31f10dff3313357ac47d37ac516692
Mesbah Mowlavi <m@ionx.ca>
2016-02-17 23:53:03 -0500

    IT COMPILES!

--- kern/syscall/write_syscall.c
+++ kern/syscall/write_syscall.c
@@ -9,7 +9,10 @@
 #include <types.h>
 #include <vnode.h>
 #include <vfs.h>
+#include <uio.h>
 #include <limits.h>
+#include <current.h>
+#include <proc.h>
 
 ssize_t sys_write(int fd, void *buf, size_t nbytes, int *error){
  	struct file *f;
@@ -18,25 +21,25 @@ ssize_t sys_write(int fd, void *buf, size_t nbytes, int *error){
  	*error = 0;
 
  	// get the file struct from the filetable
- 	*error = filetable_get(curproc->p_ft, fd, f);
+ 	*error = filetable_get(curproc->p_ft, fd, &f);
  	if (*error) return -1;
 
  	// set up the uio for writing
- 	i->iov_kbase = buf;
-	i->iov_len = nbytes;
-	u->uio_iov = i;
-	u->uio_iovcnt = 1;
-	u->uio_offset = f->f_cursor;
-	u->uio_resid = nbytes;
-	u->uio_segflg = UIO_USERSPACE;
-	u->uio_rw = UIO_WRITE;
-	u->uio_space = NULL;
+ 	i.iov_kbase = buf;
+	i.iov_len = nbytes;
+	u.uio_iov = &i;
+	u.uio_iovcnt = 1;
+	u.uio_offset = f->f_cursor;
+	u.uio_resid = nbytes;
+	u.uio_segflg = UIO_USERSPACE;
+	u.uio_rw = UIO_WRITE;
+	u.uio_space = NULL;
 
 	// acquire lock and do the write
 	lock_acquire(f->f_lock);
 	*error = VOP_WRITE(f->f_vn, &u);
-	ssize_t bytesWritten = u->uio_offset - f->f_cursor;
-	f->f_cursor = u->uio_offset;
+	ssize_t bytesWritten = u.uio_offset - f->f_cursor;
+	f->f_cursor = u.uio_offset;
 	lock_release(f->f_lock);
 
 	if (*error) return -1;
