6091a57cf022a78aa43d322fdfa55379d74f21fa
Phelan Harris <phelan@live.ca>
2016-03-06 18:29:50 -0800

    copy dat addrspace

--- kern/syscall/fork_syscall.c
+++ kern/syscall/fork_syscall.c
@@ -8,8 +8,15 @@
 #include <proctable.h>
 
 int 
-sys_fork()
+sys_fork(pid_t *pid_ret)
 {
 	// Todo
+	int ret;
+	struct addrspace *child_as;
+	ret = as_copy(curproc->p_addrspace, &child_as);
 
+	if (ret)
+		return ret;
+
+	
 }
